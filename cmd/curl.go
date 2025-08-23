e1c8a9837d2d93d435d32c8ecf8d043c1c71db3f
LiZeC <tomgreem123@gmail.com>
2025-08-23 23:07:12 +0800

    style: 调整代码

--- cmd/curl.go
+++ cmd/curl.go
@@ -153,28 +153,6 @@ func CurlCommand() *cli.Command {
 	}
 }
 
-func DoCurl(url string, timeout uint8, retry uint8) (body string, err error) {
-	req, _ := http.NewRequest("GET", url, nil)
-
-	// 覆盖默认标识，添加浏览器特征头
-	req.Header.Set("User-Agent", "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36")
-	req.Header.Set("Accept", "text/html,application/xhtml+xml")
-	req.Header.Set("Accept-Language", "en-US,en;q=0.9")
-
-	client := &http.Client{
-		Timeout: time.Duration(timeout) * time.Second,
-	}
-
-	resp, err := client.Do(req)
-	if err != nil {
-		return "", err
-	}
-	defer resp.Body.Close()
-
-	bytes, _ := io.ReadAll(resp.Body)
-	return string(bytes), nil
-}
-
 type Task struct {
 	Urls        []string
 	Concurrency uint16
@@ -188,7 +166,7 @@ type TaskRst struct {
 	Err  error
 }
 
-// TODO: 支持log能力, 详细信息可输出到log文件
+
 func DoCurlTask(task Task) (out chan TaskRst) {
 	out = make(chan TaskRst)
 	sem := make(chan struct{}, task.Concurrency)
@@ -229,4 +207,27 @@ func DoCurlTask(task Task) (out chan TaskRst) {
 
 	return out
 
+}
+
+
+func DoCurl(url string, timeout uint8, retry uint8) (body string, err error) {
+	req, _ := http.NewRequest("GET", url, nil)
+
+	// 覆盖默认标识，添加浏览器特征头
+	req.Header.Set("User-Agent", "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36")
+	req.Header.Set("Accept", "text/html,application/xhtml+xml")
+	req.Header.Set("Accept-Language", "en-US,en;q=0.9")
+
+	client := &http.Client{
+		Timeout: time.Duration(timeout) * time.Second,
+	}
+
+	resp, err := client.Do(req)
+	if err != nil {
+		return "", err
+	}
+	defer resp.Body.Close()
+
+	bytes, _ := io.ReadAll(resp.Body)
+	return string(bytes), nil
 }
